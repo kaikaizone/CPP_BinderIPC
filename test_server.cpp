be9aeeaba087d284e37814a698f3711eb0d4e728
kai.zhu <zone_kai@163.com>
2018-07-09 13:47:50 +0800

    load all code

---
+++ test_server.cpp
@@ -0,0 +1,44 @@
+
+/* 参考: frameworks\av\media\mediaserver\Main_mediaserver.cpp */
+
+#define LOG_TAG "TestService"
+//#define LOG_NDEBUG 0
+
+#include <fcntl.h>
+#include <sys/prctl.h>
+#include <sys/wait.h>
+#include <binder/IPCThreadState.h>
+#include <binder/ProcessState.h>
+#include <binder/IServiceManager.h>
+#include <cutils/properties.h>
+#include <utils/Log.h>
+
+#include "IHelloService.h"
+#include "IGoodbyeService.h"
+
+
+using namespace android;
+
+int main(void)
+{
+	/* addService */
+
+	/* while(1){ read data, 解析数据, 调用服务函数 } */
+
+	/* 打开驱动, mmap */
+	sp<ProcessState> proc(ProcessState::self());
+
+	/* 获得BpServiceManager */
+	sp<IServiceManager> sm = defaultServiceManager();
+
+	sm->addService(String16("hello"), new BnHelloService());
+	sm->addService(String16("goodbye"), new BnGoodbyeService());
+
+	/* 循环体 */
+	ProcessState::self()->startThreadPool();
+	IPCThreadState::self()->joinThreadPool();
+
+	return 0;
+}
+
+
